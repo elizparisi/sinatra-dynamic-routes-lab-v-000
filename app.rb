d7c493b48c9ecfc26f30e62d6d368370f0d3dfb9
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-11 02:02:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,9 +34,9 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
     
     if @operation == "add"
-      answer = number1 + number2
+      answer = @number1 + @number2
     elsif @operation == "subtract"
-      answer = number1 - number2
+      answer = @number1 - number2
     elsif @operation == "multiply"
       answer = number1 * number2
     elsif @operation == "divide"

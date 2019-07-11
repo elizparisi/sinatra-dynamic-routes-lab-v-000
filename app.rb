3e53ec4563e78aa24baac56cece5f07dbcacba16
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-11 00:19:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,10 @@ class App < Sinatra::Base
   get "/reversename/:name" do 
     @name = params[:name].reverse
   end
+  
+  get "/square/:number" do 
+    @number = params[:number].to_i
+    "#{number * number}"
+  end
 
 end
